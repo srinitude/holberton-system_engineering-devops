cbedad65ae7dc924ed69f73fe00889918994e1a8
Kiren <srinitude@gmail.com>
2018-01-03 01:36:41 +0000

    Adds check for the module context to be __main__

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,32 +2,33 @@
 """
 Script to retrieve an employee and his/her todo list progress
 """
-import sys
-import requests
+if __name__ == "__main__":
+    import sys
+    import requests
 
-base_url = "https://jsonplaceholder.typicode.com/"
-user_id = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com/"
+    user_id = sys.argv[1]
 
-user_url = base_url + "users/{}".format(user_id)
-todos_url = base_url + "todos?userId={}".format(user_id)
+    user_url = base_url + "users/{}".format(user_id)
+    todos_url = base_url + "todos?userId={}".format(user_id)
 
-user = requests.get(user_url).json()
-todos = requests.get(todos_url).json()
+    user = requests.get(user_url).json()
+    todos = requests.get(todos_url).json()
 
-fullname = user.get("name")
-count = 0
-total = len(todos)
+    fullname = user.get("name")
+    count = 0
+    total = len(todos)
 
-tasks = ""
+    tasks = ""
 
-for todo in todos:
-    if todo.get("completed"):
-        count += 1
-        title = todo.get("title")
-        tasks += "\t{}\n".format(title)
+    for todo in todos:
+        if todo.get("completed"):
+            count += 1
+            title = todo.get("title")
+            tasks += "\t{}\n".format(title)
 
-heading = "Employee {} is done with tasks({}/{}):".format(fullname,
-                                                          count,
-                                                          total)
-print(heading)
-print(tasks, end="")
+            heading = "Employee {} is done with tasks({}/{}):".format(fullname,
+                                                                      count,
+                                                                      total)
+    print(heading)
+    print(tasks, end="")
--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -2,35 +2,36 @@
 """
 Script to retrieve an employee and his/her todo list progress
 """
-import requests
-import json
-from collections import OrderedDict
+if __name__ == "__main__":
+    import requests
+    import json
+    from collections import OrderedDict
 
-base_url = "https://jsonplaceholder.typicode.com/"
-users_url = base_url + "users"
-users = requests.get(users_url).json()
+    base_url = "https://jsonplaceholder.typicode.com/"
+    users_url = base_url + "users"
+    users = requests.get(users_url).json()
 
-filename = "todo_all_employees.json"
-with open(filename, 'w') as f:
-    json_users = OrderedDict()
-    for user in users:
-        user_id = user.get("id")
-        username = user.get("username")
+    filename = "todo_all_employees.json"
+    with open(filename, 'w') as f:
+        json_users = OrderedDict()
+        for user in users:
+            user_id = user.get("id")
+            username = user.get("username")
 
-        todos_url = base_url + "todos?userId={}".format(user_id)
-        todos = requests.get(todos_url).json()
+            todos_url = base_url + "todos?userId={}".format(user_id)
+            todos = requests.get(todos_url).json()
 
-        json_todos = []
+            json_todos = []
 
-        for todo in todos:
-            title = todo.get("title")
-            completed = todo.get("completed")
-            todo_dict = [("username", username),
-                         ("task", title),
-                         ("completed", completed)]
-            todo_dict = OrderedDict(todo_dict)
-            json_todos.append(todo_dict)
+            for todo in todos:
+                title = todo.get("title")
+                completed = todo.get("completed")
+                todo_dict = [("username", username),
+                             ("task", title),
+                             ("completed", completed)]
+                todo_dict = OrderedDict(todo_dict)
+                json_todos.append(todo_dict)
 
-        json_users[str(user_id)] = json_todos
+            json_users[str(user_id)] = json_todos
 
-    json.dump(json_users, f)
+        json.dump(json_users, f)
