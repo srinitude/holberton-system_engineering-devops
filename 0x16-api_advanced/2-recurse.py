735be93d8e4e446bc74e42058722e801311cb2e3
Kiren <srinitude@gmail.com>
2018-01-08 23:24:11 +0000

    Subs string with variable name

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,7 +5,7 @@ Returns a list of all the hot titles in a subreddit
 import requests
 
 
-def recurse(subreddit, hot_list=[], fullname=""):
+def recurse(subreddit, hot_list=[], after=None):
     """
     Returns a list of all the hot titles in a subreddit
     """
@@ -13,8 +13,8 @@ def recurse(subreddit, hot_list=[], fullname=""):
         return None
     s = subreddit
     base_url = "https://www.reddit.com/r/{}/hot.json?limit=100".format(s)
-    if len(fullname) > 0:
-        base_url += "&after={}".format("fullname")
+    if after is not None:
+        base_url += "&after={}".format(after)
     headers = {'user-agent': 'safari:holberton/0.1.0 (by /u/srinitude)'}
     response = requests.get(base_url,
                             headers=headers,
@@ -23,8 +23,8 @@ def recurse(subreddit, hot_list=[], fullname=""):
         return None
     response = response.json()
     after = response.get("data").get("after")
-    if after is None:
-        return hot_list
     hot_posts = response.get("data").get("children")
     hot_list.extend(map(lambda p: p.get("data").get("title"), hot_posts))
+    if after is None:
+        return hot_list
     return recurse(subreddit, hot_list, after)
