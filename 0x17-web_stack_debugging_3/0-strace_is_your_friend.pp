5c8fd1ce2053f5a084c41ad2aa7e0fd153a4bf23
Kiren <srinitude@gmail.com>
2018-01-23 01:47:04 +0000

    Uses sed instead of find_line to replace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,16 +1,8 @@
 # Finds typo in wp-settings
-$module_stdlib = 'puppetlabs-stdlib'
-exec { 'puppet_module_stdlib':
-  command => "puppet module install ${module_stdlib}",
-  unless  => "puppet module list | grep ${module_stdlib}",
-  path    => ['/bin', '/usr/bin']
-}
--> file { '/var/www/html/wp-settings.php':
+file { '/var/www/html/wp-settings.php':
   ensure => present
 }
--> file_line { 'replace':
-  path    => '/var/www/html/wp-settings.php',
-  replace => true,
-  line    => "require_once( ABSPATH . WPINC . '/class-wp-locale.php' );",
-  match   => "require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );"
+-> exec {'replace':
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path    => '/bin'
 }
